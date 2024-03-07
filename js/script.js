fc61e5a1b89d62d929c4b86113116116073f0d58
ammilammmmm <sundagamingofc1@gmail.com>
2024-03-07 09:02:21 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -12,3 +12,13 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// Toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
